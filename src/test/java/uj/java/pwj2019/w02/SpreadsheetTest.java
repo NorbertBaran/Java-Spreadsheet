3f1ed4887dfe8e3c97f8aa5748af6c3644ff30cb
asokolowski <areksokolowski@gmail.com>
2019-10-19 09:13:25 +0200

    More tests added

--- src/test/java/uj/java/pwj2019/w02/SpreadsheetTest.java
+++ src/test/java/uj/java/pwj2019/w02/SpreadsheetTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -18,15 +19,21 @@ class SpreadsheetTest {
 
     @ParameterizedTest
     @MethodSource("spreadsheetInput")
-    void reverse(String[][] input, String[][] expected) {
+    void checkSpreadsheet(String[][] input, String[][] expected) {
         String[][] result = new Spreadsheet().calculate(input);
         assertThat(result).isEqualTo(expected);
     }
 
     static Stream<Arguments> spreadsheetInput() {
         return Stream.of(
-            of(readSpreadsheet("sample.txt"), readSpreadsheet("sample-result.txt"))
+            listFiles().stream()
+                .map(SpreadsheetTest::readTestCase)
+                .toArray(Arguments[]::new)
         );
+    }
+
+    static Arguments readTestCase(String caseName) {
+        return of(readSpreadsheet(caseName + ".txt"), readSpreadsheet(caseName + "-result.txt"));
     }
 
     static String[][] readSpreadsheet(String name) {
@@ -35,9 +42,30 @@ class SpreadsheetTest {
         List<String> lines = br.lines().collect(Collectors.toList());
         String[][] result = new String[lines.size()][];
         for (int i = 0; i < lines.size(); i++) {
-            result[i] = lines.get(i).split(",");
+            result[i] = buildRow(lines.get(i));
         }
         return result;
     }
 
+    static String[] buildRow(String line) {
+        List<String> result = new ArrayList<>();
+        boolean inParenthesis = false;
+        StringBuilder current = new StringBuilder();
+        for (var c: line.toCharArray()) {
+            if (c == '(') inParenthesis = true;
+            else if (c == ')') inParenthesis = false;
+            if (c == ',' && !inParenthesis) {
+                result.add(current.toString());
+                current = new StringBuilder();
+            } else {
+                current.append(c);
+            }
+        }
+        result.add(current.toString());
+        return result.toArray(new String[0]);
+    }
+
+    static List<String> listFiles() {
+        return List.of("example", "numbers", "two-timer", "walker");
+    }
 }
