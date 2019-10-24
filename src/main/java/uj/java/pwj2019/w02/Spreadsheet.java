4d75623c61c2953d1bf511dccebfdf305e5d5e4f
norbertbaran <norbertbaran1@gmail.com>
2019-10-25 01:13:16 +0200

    Faster value counting

--- src/main/java/uj/java/pwj2019/w02/Spreadsheet.java
+++ src/main/java/uj/java/pwj2019/w02/Spreadsheet.java
@@ -3,24 +3,33 @@ package uj.java.pwj2019.w02;
 public class Spreadsheet {
     private String[][] input;
 
-    private String setValue(String expr){
-        if(expr.charAt(0)=='=')
-            expr=getFormulaValue(expr);
-        else if(expr.charAt(0)=='$')
-            expr=getReferenceValue(expr);
+    private String setValue(int row, int ceil){
+        if(input[row][ceil].charAt(0)=='=')
+            input[row][ceil]=getFormulaValue(row, ceil);
+        else if(input[row][ceil].charAt(0)=='$')
+            input[row][ceil]=getReferenceValue(row, ceil);
+        return input[row][ceil];
+    }
+
+    private String getReferenceValue(int row, int ceil){
+        String reference=input[row][ceil];
+        return setValue(Integer.parseInt(reference.substring(2))-1, reference.charAt(1)-65);
+    }
+
+    private String getFormulaArgValue(String expr){
+        if(expr.charAt(0)=='$')
+            expr=setValue(Integer.parseInt(expr.substring(2))-1, expr.charAt(1)-65);
         return expr;
     }
 
-    private String getReferenceValue(String reference){
-        return setValue(input[Integer.parseInt(reference.substring(2))-1][reference.charAt(1)-65]);
-    }
+    private String getFormulaValue(int row, int ceil){
+        String formula=input[row][ceil];
 
-    private String getFormulaValue(String formula){
         String firstParam=formula.substring(5, formula.indexOf(','));
         String secondParam=formula.substring(formula.indexOf(',')+1, formula.indexOf(')'));
 
-        int firstValue= Integer.parseInt(setValue(firstParam));
-        int secondValue= Integer.parseInt(setValue(secondParam));
+        int firstValue= Integer.parseInt(getFormulaArgValue(firstParam));
+        int secondValue= Integer.parseInt(getFormulaArgValue(secondParam));
 
         switch (formula.substring(1,4)){
             case "ADD":
@@ -39,23 +48,10 @@ public class Spreadsheet {
     }
 
     public String[][] calculate(String[][] input) {
-        this.input=input;
+        this.input=input.clone();
         for (int row = 0; row < input.length; row++)
             for (int ceil = 0; ceil < input[row].length; ceil++)
-                input[row][ceil]=setValue(input[row][ceil]);
-        return input;
+                setValue(row, ceil);
+        return this.input;
     }
-
-    /*public static void main(String[] args) {
-        String input[][]={{"1", "2", "3"}, {"4", "5", "6"}, {"$A1", "$C1", "$B3"}, {"=ADD(10,$A1)", "=SUB($C3,$A1)", "0"}};
-        //String test="test123";
-        //System.out.println(test.substring(1,3));
-        var spreadsheet=new Spreadsheet();
-        input=spreadsheet.calculate(input);
-        for(var row: input){
-            for(var ceil: row)
-                System.out.print(ceil+"; ");
-            System.out.println();
-        }
-    }*/
 }
